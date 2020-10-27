5a797b38e52e887da6437ef72006eab3f5d8aab9
Kay <69727381+hallokay@users.noreply.github.com>
2020-10-27 21:55:30 +0900

    10.27 --//footer //header-js //media 768px

--- app.js
+++ app.js
@@ -3,6 +3,11 @@
 const header = document.querySelector('.header');
 const menuBtn = document.querySelector('.hamburger_btn');
 
+window.addEventListener('scroll',function(){
+    let windowPosition = window.scrollY > 0;
+    header.classList.toggle('active',windowPosition)
+})
+
 menuBtn.addEventListener('click',function(){
 header.classList.toggle('menu_open'); 
 
